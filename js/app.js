063286087e9b97fac54cc69871aef8cf93fa34d8
Vik Akam <vikramakam@hotmail.com>
2019-11-11 21:30:59 -0800

    fixed counters, added results rendering, done with lab11

--- js/app.js
+++ js/app.js
@@ -1,19 +1,20 @@
 'use strict';
 
 // Week 3 Day 1: 
-/* constructor function for objects (name, path) :)
-algorithm to random generate 3 unique products from img directory
-event listener :)
-upon click, generate three new products to pick from
-in constructor, define property to hold number of times product been clicked and update when clicked
-control 25 rounds of clicking, stop clicking ability after 25
-view report of results of each image's clicks + views (seen but not clicked) */
-
-// global counter for max number of choices
-var totalClicks = 0;
+/* 
+- constructor function for objects (name, path) :)
+- algorithm to random generate 3 unique products from img directory :)
+- upon click, generate three new products to pick from
+in constructor, define property to hold number of times product been clicked and update when clicked :)
+- control 25 rounds of clicking, stop clicking ability after 25 :)
+- view report of results of each image's clicks + views :) 
+*/
 
 // global array for inserted products to choose from
 var displayArray = [];
+
+var productsConsidered = [];
+
 
 // constructor for products
 function Product(name, path) {
@@ -22,17 +23,23 @@ function Product(name, path) {
   this.clicked = 0;
   this.viewed = 0;
 
-  // once values are inputted, push product into product array to call from
+  // push product into a product array to call from
   productStorage.push(this);
 
+  // increment clicked vs. viewed products
   this.productClicks = function () {
     this.clicked++;
   }
-
   this.productViews = function () {
     this.viewed++;
   }
+
+  // keep track of considered products in an array
+  this.addToConsidered = function () {
+    productsConsidered.push(this);
+  }
 }
+
 
 // empty array for storing products
 var productStorage = [];
@@ -59,11 +66,10 @@ var Usb = new Product('Usb', './img/usb.gif');
 var WaterCan = new Product('Water Can', './img/water-can.jpg');
 var WineGlass = new Product('Wine Glass', './img/wine-glass.jpg');
 
-
-// function to obtain 3 products and render them to HTML
+// obtain 3 products and render them to HTML
 function get3RandomProductsAndRender() {
 
-  // set temp array to nothing
+  // set/reset temp array to empty
   displayArray = [];
 
   // push 3 products at random into temporary array 3 times
@@ -82,20 +88,47 @@ function get3RandomProductsAndRender() {
   var placeholder0 = document.getElementById('choice-0');
   var placeholder1 = document.getElementById('choice-1');
   var placeholder2 = document.getElementById('choice-2');
-  placeholder0.src = displayArray[0].path;
-  displayArray[0].productViews();
-  placeholder1.src = displayArray[1].path;
-  displayArray[0].productViews();
-  placeholder2.src = displayArray[2].path;
-  displayArray[0].productViews();
+
+  function productViewCount() {
+    placeholder0.src = displayArray[0].path;
+    displayArray[0].productViews();
+    placeholder1.src = displayArray[1].path;
+    displayArray[1].productViews();
+    placeholder2.src = displayArray[2].path;
+    displayArray[2].productViews();
+  }
+  productViewCount();
 }
 
 get3RandomProductsAndRender();
 
-// upon clicking an image, increment counter for clicks only for the clicked product, then reset with 3 new products
+// render totals as unordered
+function renderTotals(domReferenceResults, productsConsidered) {
+  var headerList = document.createElement('h3');
+  headerList.textContent = 'Results';
+  domReferenceResults.append(headerList);
+
+  var ul = document.createElement('ul');
+
+  for (var storageIndex = 0; storageIndex < productsConsidered.length; storageIndex++) {
+    var li = document.createElement('li');
+    li.textContent = `${productsConsidered[storageIndex].name} had ${productsConsidered[storageIndex].clicked} votes and was shown ${productsConsidered[storageIndex].viewed} times.`;
+    ul.append(li);
+  }
+
+  domReferenceResults.append(ul);
+
+}
+
+
+// global counter for max number of choices
+var totalClicks = 0;
+const MAX_CLICKS = 25;
+
+// upon clicking an image, increment clicks and add to considered list only for the clicked product, then reset with 3 new products
 function clickManager(event) {
   totalClicks++;
-  if (totalClicks < 25) {
+  if (totalClicks < MAX_CLICKS) {
     var displayArrayIndex;
 
     if (event.target.id === 'choice-0') {
@@ -108,11 +141,28 @@ function clickManager(event) {
       displayArrayIndex = 2;
     }
 
-    displayArray[displayArrayIndex].productClicks();
+    var product = displayArray[displayArrayIndex];
+    console.log(product);
+
+    if (product.clicked === 0) {
+      product.addToConsidered();
+      console.log(productsConsidered);
+    }
     console.log(productStorage);
+
+    product.productClicks();
+    console.log(product.clicked);
+
   }
   else {
+    // stop clicking ability
+    placeholder0.removeEventListener('click', clickManager);
+    placeholder1.removeEventListener('click', clickManager);
+    placeholder2.removeEventListener('click', clickManager);
 
+    // show total product clicks vs. views
+    var domReferenceResults = document.getElementById('results');
+    renderTotals(domReferenceResults, productsConsidered);
   }
 
   get3RandomProductsAndRender();
